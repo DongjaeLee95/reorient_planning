beea8da8fd0784fb0c0c20a7b579d1673922564e
dongjaelee <ehdwo713@snu.ac.kr>
2024-03-17 16:23:23 +0900

    erase epsilon

---
+++ scripts_acados/main_v2.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+import numpy as np
+import math
+import reorient_planning_v2
+import math_lib
+import pytictoc
+import time
+import matplotlib.pyplot as plt
+import param as param_class
+import matplotlib.pyplot as plt
+from plot_result_v2 import plot
+
+if __name__ == '__main__':
+    
+    deg2rad = math.pi/180.0
+    rad2deg = 180.0/math.pi
+
+    dt = 0.01
+    ur_max = 16.0*np.ones((6,1))
+    ur_min = np.zeros((6,1))
+    phidot_lb = deg2rad*-30.0
+    phidot_ub = deg2rad*30.0
+    m = 2.8
+    g = 9.81
+    mu_dot = 1.0
+    L = 0.258
+    kf = 0.016
+    tilt_ang = deg2rad*30
+    Am = np.zeros((6,6))
+    Am_inv = np.zeros((6,6))
+
+    param_ = param_class.Param(dt=dt, Am=Am, Am_inv=Am_inv, ur_max=ur_max, ur_min=ur_min,
+                               phidot_lb=phidot_lb, phidot_ub=phidot_ub, m=m, g=g, mu_dot=mu_dot, 
+                               L=L, kf=kf, tilt_ang=tilt_ang)
+
+    reorient_plan_obj = reorient_planning_v2.reorient_planning(param = param_)
+    param_.Am = reorient_plan_obj.set_Am(param_)
+    param_.Am_inv = np.linalg.inv(param_.Am)
+
+    tf = 20.0
+    x0 = np.zeros((2))
+    dt_ = param_.dt
+    Nsim = int(tf/dt_)
+    ts = np.linspace(0.0,tf,Nsim+1)
+
+    ### algorithm input: 
+    rho = 0.5
+    nParam = 12 # Rf, tau, phi, phi_d
+    param_val = np.zeros((Nsim,nParam))
+    Rf = np.zeros((Nsim,3))
+    tau = np.zeros((Nsim,3))
+    phi_d = np.zeros((Nsim,3))
+    phi = np.zeros((Nsim+1,3))
+    for k in range(Nsim):
+        if k < int(tf/(2.0*dt_)):
+            Rf[k,:] = np.array([rho*2.0/tf*ts[k]*param_.m*param_.g,
+                                -rho*2.0/tf*ts[k]*param_.m*param_.g,
+                                param_.m*param_.g])
+        else:
+            Rf[k,:] = np.array([rho*param_.m*param_.g,
+                                -rho*param_.m*param_.g,
+                                param_.m*param_.g])
+        # Rf = np.array([[rho*m_*g_],[0.0],[m_*g_]])
+        tau[k,:] = np.zeros((1,3))
+
+    plt.figure()
+    for i in range(3):
+        plt.subplot(3,1,i+1)
+        plt.plot(ts,np.append(Rf[0,i], Rf[:,i]),color='black',marker='o',linewidth=2)
+        plt.grid(True)
+        plt.xlabel("time [s]")
+    plt.show()
+
+    ### CLOSED-LOOP SIMULATION
+    ocp_solver, integrator = reorient_plan_obj.init_ocp(x0,param_val[0,:])
+
+    # ocp_solver, integrator = reorient_plan_obj.set_ocp_param(param_val[0,:])
+    ### MAY NEED TO INITIALIZE! ###
+
+    nx = ocp_solver.acados_ocp.dims.nx
+    nu = ocp_solver.acados_ocp.dims.nu
+    N = ocp_solver.acados_ocp.dims.N
+
+    simX = np.ndarray((Nsim+1,nx))
+    simU = np.ndarray((Nsim,nu))
+    t_compt = np.zeros((Nsim))
+    simX[0,:] = x0
+
+    for k in range(Nsim):
+        print("step: ", k)
+
+        phi[k,0:2] = simX[k,0:2] # Assumption: phi_r = phi
+        phi[k,2] = phi_d[k,2]
+
+        param_val[k,:] = np.hstack((Rf[k,:], tau[k,:], phi[k,:], phi_d[k,:]))
+        
+        for kk in range(N+1):
+            ocp_solver.set(kk, "p", param_val[k,:])
+        # ocp_solver, integrator = reorient_plan_obj.set_ocp_param(param_val[k,:])
+
+        simU[k,:] = ocp_solver.solve_for_x0(x0_bar = simX[k,:])
+        t_compt[k] = ocp_solver.get_stats('time_tot')
+
+        # simulate system
+        simX[k+1,:] = integrator.simulate(x=simX[k,:],u=simU[k,:]) ## FROM HERE!
+    
+    plot(ts, simU, simX, t_compt, latexify=False, plt_show=True )
---
+++ scripts_acados/reorient_planning_v2.py
@@ -0,0 +1,232 @@
+#!/usr/bin/env python3
+import numpy as np
+# import math
+# import pytictoc
+import math_lib
+import math
+import sys
+sys.path.append("/home/dj/acados/interfaces/acados_template/acados_template")
+from acados_template import AcadosOcp, AcadosOcpSolver, AcadosModel, AcadosSimSolver
+import traceback
+from casadi import SX, vertcat, blockcat, power, dot, simplify
+import param as param_class
+
+"""
+Modification from the original code
+1) erase epsilon 
+"""
+
+""" OCP formulation
+# state variable: x = [phi_r1, phi_r2]
+# input variable: u = [alpha, beta]
+# algorithm input: 
+#   x_prev      : previous step x
+#   phi_d       : unmodified orientation reference
+#   R           : rotation matrix
+#   ur          : rotor thrust, ur=[ur1,...,ur6]
+
+# output:
+#   x,u
+
+# parameter:
+#   dt          : discrete time interval
+#   Am          : ctrl allocation matrix
+#   ur_max      : rotor thurst maximum   
+#   ur_min      : rotor thrust minimum
+#   phidot_lb   : lower bound of alpha, beta
+#   phidot_ub   : upper bound of alpha, beta
+#   m           : mass of the multirotor
+#   g           : gravitational accel.
+#   mu_dot      : J_reg_phidot weight
+#   mu_ddot     : J_reg_phiddot weight
+"""
+
+class reorient_planning:
+    def __init__(self, param):
+        # self.param = param_class.Param()
+        self.param = param
+
+        self.param.Am = self.set_Am(param)
+        self.param.Am_inv = np.linalg.inv(self.param.Am)
+
+        self.ocp = AcadosOcp()
+
+    def set_Am(self,param): 
+        L = param.L
+        kf = param.kf
+        tilt_ang = param.tilt_ang
+        ca = math.cos(tilt_ang)
+        sa = math.sin(tilt_ang)
+
+        P1 = L*ca - kf*sa
+        P2 = L*sa + kf*ca
+        return np.array(
+            [ [-0.5*sa, -0.5*sa, sa, -0.5*sa, -0.5*sa, sa],
+            [-math.sqrt(3)/2*sa, math.sqrt(3)/2*sa, 0, -math.sqrt(3)/2*sa, math.sqrt(3)/2*sa, 0],
+            [ca, ca, ca, ca, ca, ca],
+            [-0.5*P1, 0.5*P1, P1, 0.5*P1, -0.5*P1, -P1],
+            [-math.sqrt(3)/2*P1, -math.sqrt(3)/2*P1, 0, math.sqrt(3)/2*P1, math.sqrt(3)/2*P1, 0],
+            [-P2, P2, -P2, P2, -P2, P2]
+            ]
+        )
+
+    # def set_ocp_param(self,param_val):
+    #     self.ocp.parameter_values = param_val # [] Rf, tau, phi, phi_d
+
+    #     solver_json = 'acados_ocp_' + self.ocp.model.name + '.json'
+    #     acados_ocp_solver = AcadosOcpSolver(self.ocp, json_file = solver_json)
+    #     acados_integrator = AcadosSimSolver(self.ocp, json_file = solver_json)
+
+    #     return acados_ocp_solver, acados_integrator
+
+    def init_ocp(self,x0,param_val): # called only once
+        # parameter handle
+        param_ = self.param
+
+        self.ocp.model = self.ocp_model()
+        self.ocp.dims.N = 1
+
+        ############### CONSTRAINT ###############
+        deg2rad = math.pi/180.0
+        x1 = deg2rad*26.3819
+        y1 = deg2rad*14.3216
+        y2 = deg2rad*-29.6985
+
+        # 1. input lower & upper bounds
+        self.ocp.constraints.lbu = np.array([[param_.phidot_lb],[param_.phidot_lb]]).flatten()
+        self.ocp.constraints.ubu = np.array([[param_.phidot_ub],[param_.phidot_ub]]).flatten()
+        self.ocp.constraints.idxbu = np.array([0, 1])
+
+        # 2. hoverability cosntraint approximation
+        self.ocp.constraints.C_e = np.array([[0.0, 1.0],
+                                        [(y1-y2)/x1, -1.0], 
+                                        [-(y1-y2)/x1, -1.0]])
+        self.ocp.constraints.lg_e = np.array([[y2], [-(2.0*y1-y2)], [-(2.0*y1-y2)]]).flatten()
+        self.ocp.constraints.ug_e = np.array([[y1], [-y2], [-y2]]).flatten()
+        # 3. rotor thrust min & max constraint
+        self.ocp.constraints.lh_e = param_.ur_min.flatten()
+        self.ocp.constraints.uh_e = param_.ur_max.flatten()
+
+        # 4. initial value
+        self.ocp.constraints.x0 = x0
+
+        ############### COST OPTION ###############
+        self.ocp.cost.cost_type = 'EXTERNAL'
+        self.ocp.cost.cost_type_e = 'EXTERNAL'
+
+        ############### SOLVER OPTION ###############
+        self.ocp.solver_options.qp_solver = 'FULL_CONDENSING_QPOASES' # FULL_CONDENSING_QPOASES
+            # PARTIAL_CONDENSING_HPIPM, FULL_CONDENSING_QPOASES, FULL_CONDENSING_HPIPM,
+            # PARTIAL_CONDENSING_QPDUNES, PARTIAL_CONDENSING_OSQP, FULL_CONDENSING_DAQP
+        # ocp.solver_options.hessian_approx = 'GAUSS_NEWTON' # 'GAUSS_NEWTON', 'EXACT'
+        self.ocp.solver_options.ext_cost_num_hess = 0 # EXACT hessian if 0 
+        self.ocp.solver_options.integrator_type = 'ERK' # ERK (Euler), IRK, GNSF, DISCRETE
+        # ocp.solver_options.print_level = 1
+        self.ocp.solver_options.nlp_solver_type = 'SQP' # SQP_RTI, SQP
+        self.ocp.solver_options.tf = self.param.dt
+
+        self.ocp.solver_options.sim_method_newton_iter = 10
+        self.ocp.solver_options.qp_solver_cond_N = 1
+
+        self.ocp.parameter_values = param_val # [] Rf, tau, phi, phi_d
+
+        solver_json = 'acados_ocp_' + self.ocp.model.name + '.json'
+        acados_ocp_solver = AcadosOcpSolver(self.ocp, json_file = solver_json)
+        acados_integrator = AcadosSimSolver(self.ocp, json_file = solver_json)
+
+        return acados_ocp_solver, acados_integrator
+
+    def ocp_model(self) -> AcadosModel:
+
+        model_name = 'reorient_model'
+
+        # set up states & controls
+        phi_r1 = SX.sym('phi_r1')
+        phi_r2 = SX.sym('phi_r2')
+
+        x = vertcat(phi_r1,phi_r2)
+
+        alpha = SX.sym('alpha')
+        beta = SX.sym('beta')
+
+        u = vertcat(alpha,beta)
+
+        # xdot
+        phi_r1_dot = SX.sym('phi_r1_dot')
+        phi_r2_dot = SX.sym('phi_r2_dot')
+
+        xdot = vertcat(phi_r1_dot,phi_r2_dot)
+
+        # dynamics
+        f_expl = vertcat(alpha, beta)
+        f_impl = xdot - f_expl
+
+        # set up parameters
+        rf1 = SX.sym("rf1") # force 1
+        rf2 = SX.sym("rf2") # force 2
+        rf3 = SX.sym("rf3") # force 3
+        tau1 = SX.sym("tau1") # torque 1
+        tau2 = SX.sym("tau2") # torque 2
+        tau3 = SX.sym("tau3") # torque 3
+        phi_d1 = SX.sym("phi_d1") # phi_d1
+        phi_d2 = SX.sym("phi_d2") # phi_d2
+        phi_d3 = SX.sym("phi_d3") # phi_d3
+        phi1 = SX.sym("phi1") # phi1
+        phi2 = SX.sym("phi2") # phi2
+        phi3 = SX.sym("phi3") # phi3
+        p = vertcat(rf1,rf2,rf3,
+                    tau1,tau2,tau3,
+                    phi1,phi2,phi3,
+                    phi_d1,phi_d2,phi_d3)
+
+        # nonlinear (terminal) constraints
+        Rf = vertcat(rf1,rf2,rf3)
+        tau = vertcat(tau1,tau2,tau3)
+        phi = vertcat(phi1,phi2,phi3)
+        cm = math_lib.math_lib_wCasadi()
+        R = cm.Rzyx_casadi(phi)
+
+        phi_r = vertcat(phi_r1,phi_r2,phi_d3)
+        Rc = cm.Rzyx_casadi(phi_r)
+        param_ = self.param
+        ustar = param_.Am_inv @ ( vertcat( (Rc.T @ Rf), tau ) )
+        con_h_expr_e = ustar
+
+        ############### COST ###############
+        # u_casadi = (param_.Am_inv @ vertcat(R.T@Rf, tau))
+
+        # J_phi: orientation error w.r.t. phi_d
+        phi_re = vertcat(phi_r1-phi_d1,phi_r2-phi_d2,0.0)
+        J_phi = dot(phi_re,phi_re)
+
+        # J_u: avoid rotor saturation
+        # ustar = param_.Am_inv @ ( blockcat(Rc.T@R, np.zeros((3,3)), np.zeros((3,3)), np.eye(3)) @ param_.Am @ u_casadi )
+
+        temp = 0
+        for k in range(6):
+            temp = temp + power( (2.0/(param_.ur_max[k] - param_.ur_min[k])) * (ustar[k] - (param_.ur_min[k] + param_.ur_max[k])/2.0), 6)
+
+        J_u = 1.0/6.0*temp
+        
+        # J_reg_phidot: phi_r variation regulation
+        J_reg_phidot = param_.mu_dot*( power(alpha,2) + power(beta,2))
+
+        J = J_reg_phidot            # running cost
+        J_e = J_phi + J_u           # terminal cost
+
+        # set model
+        model = AcadosModel()
+
+        model.f_impl_expr = f_impl
+        model.f_expl_expr = f_expl
+        model.x = x
+        model.xdot = xdot
+        model.u = u
+        model.p = p
+        model.name = model_name
+        model.con_h_expr_e = con_h_expr_e
+        # set cost
+        model.cost_expr_ext_cost = J
+        model.cost_expr_ext_cost_e = J_e
+
+        return model
